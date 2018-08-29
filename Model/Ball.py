d142919d11a0b176d75e29420bab499ed666f570
nhatvu <rains.on.stonewall@gmail.com>
2018-08-29 12:15:50 -0400

    collide

--- Model/Ball.py
+++ Model/Ball.py
@@ -3,10 +3,11 @@ import pygame
 
 class Ball(object):
 
-    def __init__(self, coor, radius):
+    def __init__(self, coor, radius, thick):
         self.radius = radius
         self.x = coor[0]
         self.y = coor[1]
+        self.thick = thick
 
-    def display(self, surface, color, thickness):
-        pygame.draw.circle(surface, color, [self.x, self.y], self.radius, thickness)
+    def display(self, surface, color):
+        pygame.draw.circle(surface, color, [self.x, self.y], self.radius, self.thick)
--- Model/Paddle.py
+++ Model/Paddle.py
@@ -3,13 +3,17 @@ import pygame
 
 class Paddle(object):
 
-    def __init__(self, top, left, width, length):
+    def __init__(self, top, left, width, length, thick):
         self.x = top
         self.y = left
         self.width = width
         self.length = length
-        self.center_x = self.x + self.width / 2
-        self.center_y = self.y + self.length / 2
+        self.thick = thick
 
-    def display(self, surface, color, thickness):
-        pygame.draw.rect(surface, color, [self.x, self. y, self.width, self.length], thickness)
+    def center_coor(self):
+        center_x = self.x + self.width / 2
+        center_y = self.y + self.length / 2
+        return [center_x, center_y]
+
+    def display(self, surface, color):
+        pygame.draw.rect(surface, color, [self.x, self. y, self.width, self.length], self.thick)
